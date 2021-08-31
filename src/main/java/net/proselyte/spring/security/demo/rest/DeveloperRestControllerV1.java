7f8c5ee520ae9f0e164e0eed850c01d06c79103c
user <kavkras83@mail.ru>
2021-08-31 20:19:27 +0300

    refactoring: интеграция части шаблонного проекта

--- src/main/java/net/proselyte/spring/security/demo/rest/DeveloperRestControllerV1.java
+++ src/main/java/net/proselyte/spring/security/demo/rest/DeveloperRestControllerV1.java
@@ -5,10 +5,31 @@
 package net.proselyte.spring.security.demo.rest;
 
 
+import net.proselyte.spring.security.demo.model.Developer;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.*;
 
 @RestController
 @RequestMapping("/api/v1/developers")
 public class DeveloperRestControllerV1 {
 
+	private List<Developer> DEVELOPERS = Stream.of(
+		new Developer(1L, "Ivan", "Ivanov"),
+		new Developer(2L, "Sergey", "Sergeev"),
+		new Developer(3L, "Petr", "Petrov")
+	).collect(Collectors.toList());
+
+	@GetMapping
+	public List<Developer> getAll() {
+		return DEVELOPERS;
+	}
+
+	@GetMapping("/{id}")
+	public Developer getById(@PathVariable Long id) {
+		return DEVELOPERS.stream().filter(developer -> developer.getId().equals(id))
+			.findFirst()
+			.orElse(null);
+	}
 }
