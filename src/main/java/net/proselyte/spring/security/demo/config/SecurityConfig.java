0b1034401bbee507dab3789a7502f6240a7fbfbb
user <kavkras83@mail.ru>
2021-08-31 20:36:39 +0300

    refactoring: интеграция spring security

---
+++ src/main/java/net/proselyte/spring/security/demo/config/SecurityConfig.java
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) 2021 Tander, All Rights Reserved.
+ */
+
+package net.proselyte.spring.security.demo.config;
+
+/**
+ * Класс SecurityConfig
+ */
+public class SecurityConfig {
+
+
+}
