ba0a9de9846fc2676dbb16d21d71e68d10cb844b
user <kavkras83@mail.ru>
2021-09-05 18:05:48 +0300

    refactoring: STEP_6 - использование аннотации @PreAuthorization

--- src/main/java/net/proselyte/spring/security/demo/config/SecurityConfig.java
+++ src/main/java/net/proselyte/spring/security/demo/config/SecurityConfig.java
@@ -5,9 +5,8 @@
 package net.proselyte.spring.security.demo.config;
 
 import net.proselyte.spring.security.demo.model.Role;
-import net.proselyte.spring.security.demo.model.*;
 import org.springframework.context.annotation.*;
-import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.*;
 import org.springframework.security.core.userdetails.*;
@@ -17,6 +16,7 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
@@ -24,9 +24,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 			.csrf().disable()
 			.authorizeRequests()
 			.antMatchers("/").permitAll()
-			.antMatchers(HttpMethod.GET, "/api/**").hasAuthority(Permission.DEVELOPERS_READ.getPermission())
-			.antMatchers(HttpMethod.POST, "/api/**").hasAuthority(Permission.DEVELOPERS_WRITE.getPermission())
-			.antMatchers(HttpMethod.DELETE, "/api/**").hasAuthority(Permission.DEVELOPERS_WRITE.getPermission())
 			.anyRequest()
 			.authenticated()
 			.and()
@@ -38,21 +35,18 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	protected UserDetailsService userDetailsService() {
 		return new InMemoryUserDetailsManager(
 			User.builder().username("admin")
-				// Use without encode first
 				.password(passwordEncoder().encode("admin"))
 				.authorities(Role.ADMIN.getAuthorities())
 				.build(),
 			User.builder().username("user")
-				// Use without encode first
 				.password(passwordEncoder().encode("user"))
 				.authorities(Role.USER.getAuthorities())
 				.build()
 		);
-		// Go to UserDetailsServiceImpl - InMemory
 	}
 
 	@Bean
-	protected PasswordEncoder passwordEncoder(){
+	protected PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder(12);
 	}
 }
--- src/main/java/net/proselyte/spring/security/demo/rest/DeveloperRestControllerV1.java
+++ src/main/java/net/proselyte/spring/security/demo/rest/DeveloperRestControllerV1.java
@@ -5,18 +5,20 @@
 package net.proselyte.spring.security.demo.rest;
 
 import net.proselyte.spring.security.demo.model.Developer;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.stream.*;
 
 @RestController
 @RequestMapping("/api/v1/developers")
 public class DeveloperRestControllerV1 {
-	private final List<Developer> DEVELOPERS = List.of(
+	private final List<Developer> DEVELOPERS = Stream.of(
 		new Developer(1L, "Ivan", "Ivanov"),
 		new Developer(2L, "Kolya", "Ivanov"),
 		new Developer(3L, "Sergey", "Sergeev")
-	);
+	).collect(Collectors.toList());
 
 	@GetMapping
 	public List<Developer> getAll() {
@@ -24,6 +26,7 @@ public class DeveloperRestControllerV1 {
 	}
 
 	@GetMapping("/{id}")
+	@PreAuthorize("hasAuthority('developers:read')")
 	public Developer getById(@PathVariable Long id) {
 		return DEVELOPERS.stream()
 			.filter(developer -> developer.getId().equals(id))
@@ -32,12 +35,14 @@ public class DeveloperRestControllerV1 {
 	}
 
 	@PostMapping
+	@PreAuthorize("hasAuthority('developers:write')")
 	public Developer create(@RequestBody Developer developer) {
 		this.DEVELOPERS.add(developer);
 		return developer;
 	}
 
 	@DeleteMapping("/{id}")
+	@PreAuthorize("hasAuthority('developers:write')")
 	public void deleteById(@PathVariable Long id) {
 		this.DEVELOPERS.removeIf(developer -> developer.getId().equals(id));
 	}
